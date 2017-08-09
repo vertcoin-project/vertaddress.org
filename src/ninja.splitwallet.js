3fad68bc64fde1f713b6c0680b60e4c2aa12d948
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2017-08-10 00:25:52 +0200

    Few remaining rebrandings

--- src/ninja.splitwallet.js
+++ src/ninja.splitwallet.js
@@ -55,7 +55,7 @@ ninja.wallets.splitwallet = {
 			var output = document.createElement("div");
 			output.setAttribute("id", "splitoutput");
 			var m = {};
-			output.appendChild(this.mkOutputRow(bitcoinAddress, "split_addr", "Bitcoin Address:    "));
+			output.appendChild(this.mkOutputRow(bitcoinAddress, "split_addr", "Vertcoin Address:    "));
 			m["split_addr"] = bitcoinAddress;
 
 			for (var i = 0; i < shares.length; i++) {
